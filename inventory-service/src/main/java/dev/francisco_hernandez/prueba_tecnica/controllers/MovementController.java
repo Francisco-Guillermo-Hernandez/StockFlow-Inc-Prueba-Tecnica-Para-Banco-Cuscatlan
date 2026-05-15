70fbb4d5aab18a440c10652a10a0140101fea5bc
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-15 01:40:30 -0600

    ft: se agregaron methodos al movement controller

--- inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/controllers/MovementController.java
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/controllers/MovementController.java
@@ -1,10 +1,18 @@
 package dev.francisco_hernandez.prueba_tecnica.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/movements")
 class MovementController {
 
+    @PostMapping
+    public String register() {
+        return "hello world";
+    }
+
+    @GetMapping("{productId}/history")
+    public String history(@PathVariable String productId) {
+        return "hello world";
+    }
 }
