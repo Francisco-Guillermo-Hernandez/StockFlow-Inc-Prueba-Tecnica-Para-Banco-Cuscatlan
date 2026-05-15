748bbe1fed7eb46b25d099f524fb7f6736fad59c
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-15 01:27:02 -0600

    se inicializo el controlador Products con un methodo

--- inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/controllers/ProductsController.java
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/controllers/ProductsController.java
@@ -1,10 +1,16 @@
 package dev.francisco_hernandez.prueba_tecnica.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping("/")
-class ControllerController {
+@RequestMapping("/products")
+class ProductsController {
 
+    @GetMapping("/{id}")
+    public String productById(@PathVariable String id) {
+        return  "hello world";
+    }
 }
