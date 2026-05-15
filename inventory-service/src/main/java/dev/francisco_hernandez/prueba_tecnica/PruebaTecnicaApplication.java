e1e6e9fd8889ee9ac72250709c6fdcc426760423
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-15 00:56:57 -0600

    Initial commit

---
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/PruebaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package dev.francisco_hernandez.prueba_tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaApplication.class, args);
+	}
+
+}
---
+++ inventory-service/src/test/java/dev/francisco_hernandez/prueba_tecnica/PruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.francisco_hernandez.prueba_tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
