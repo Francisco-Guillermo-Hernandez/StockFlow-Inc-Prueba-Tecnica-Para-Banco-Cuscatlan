9839a6b91733c4bc576ce532131c3661ff9235a5
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-15 01:39:48 -0600

    ft: se agregaron mas methodos

--- inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/controllers/ProductsController.java
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/controllers/ProductsController.java
@@ -1,16 +1,28 @@
 package dev.francisco_hernandez.prueba_tecnica.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import dev.francisco_hernandez.prueba_tecnica.entities.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 class ProductsController {
 
+    @GetMapping
+    public List<Product> getProducts() {
+        List<Product> products = new ArrayList<>();
+        return products;
+    }
+
     @GetMapping("/{id}")
     public String productById(@PathVariable String id) {
         return  "hello world";
+    }
+
+    @PostMapping
+    public String addProduct(@RequestBody Product product) {
+        return "hello world";
     }
 }
