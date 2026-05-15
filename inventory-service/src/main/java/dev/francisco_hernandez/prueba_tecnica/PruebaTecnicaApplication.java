bab2608615b7ccb7b54508b743c28feec5779068
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-15 01:29:20 -0600

    ft: se desactivo el login por defecto que se agrega por spring security

--- inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/PruebaTecnicaApplication.java
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/PruebaTecnicaApplication.java
@@ -2,8 +2,9 @@ package dev.francisco_hernandez.prueba_tecnica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "dev.francisco_hernandez.prueba_tecnica", exclude = SecurityAutoConfiguration.class)
 public class PruebaTecnicaApplication {
 
 	public static void main(String[] args) {
