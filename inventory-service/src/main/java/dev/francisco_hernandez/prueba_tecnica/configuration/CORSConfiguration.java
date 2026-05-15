ef31eae7a47dda7aabb97cc44a2eeecf7ca5cbf7
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-15 01:25:47 -0600

    ft: se agrego una configuracion de CORS para poder usar el servicio con la SPA

--- inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/configuration/CORSConfiguration.java
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/configuration/CORSConfiguration.java
@@ -1,4 +1,19 @@
 package dev.francisco_hernandez.prueba_tecnica.configuration;
 
-public class CORSConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
