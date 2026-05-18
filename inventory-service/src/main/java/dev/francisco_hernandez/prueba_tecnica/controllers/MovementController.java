2a5c3d327e5c148b967435870d1ccd1d6d807d41
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-18 06:25:03 -0600

    ft: se implementaron los methodos en el controlador

--- inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/controllers/MovementController.java
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/controllers/MovementController.java
@@ -1,18 +1,41 @@
 package dev.francisco_hernandez.prueba_tecnica.controllers;
 
+import dev.francisco_hernandez.prueba_tecnica.dto.MovementDto;
+import dev.francisco_hernandez.prueba_tecnica.entities.Movement;
+import dev.francisco_hernandez.prueba_tecnica.mapper.MovementMapper;
+import dev.francisco_hernandez.prueba_tecnica.service.MovementService;
 import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movements")
-class MovementController {
+public class MovementController {
 
-    @PostMapping
-    public String register() {
-        return "hello world";
+    private final MovementService service;
+    private final MovementMapper movementMapper;
+
+    public MovementController(MovementService service, MovementMapper movementMapper) {
+        this.service = service;
+        this.movementMapper = movementMapper;
     }
 
-    @GetMapping("{productId}/history")
-    public String history(@PathVariable String productId) {
-        return "hello world";
+    @PostMapping("/")
+    public Movement register(@Valid @RequestBody MovementDto movementDto) {
+        Movement movement = movementMapper.toEntity(movementDto);
+        return service.createAMovement(movement);
     }
+
+    @GetMapping("/{productId}/history")
+    public List<Movement> history(@PathVariable Long productId) {
+       return service.listAllByProductId(productId);
+    }
+
+
+    @GetMapping("/")
+    public List<Movement> listMovements() {
+        return service.listAll();
+    }
+
 }
