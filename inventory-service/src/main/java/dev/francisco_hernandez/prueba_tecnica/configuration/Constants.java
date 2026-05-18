e365f166c173183ad130651865bfc901421f440d
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-18 12:26:33 -0600

    ft: se agregaron constantes

--- inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/configuration/Constants.java
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/configuration/Constants.java
@@ -1,4 +1,11 @@
 package dev.francisco_hernandez.prueba_tecnica.configuration;
 
 public class Constants {
+
+    public static final String INVALID_REQUEST_CODE = "customMessages.invalidRequest.code";
+    public static final String INVALID_REQUEST_MESSAGE = "customMessages.invalidRequest.message";
+
+    public static final String NOT_FOUND_REQUEST_CODE = "customMessages.notFound.code";
+    public static final String NOT_FOUND_REQUEST_MESSAGE = "customMessages.notFound.message";
+
 }
