83fa66d6ec03bbaf0b2880c08d10ca04fc41ae6a
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-18 06:22:56 -0600

    ft: Se renombro la implementacion

--- inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/exceptions/MethodArgumentNotValidException.java
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/exceptions/MethodArgumentNotValidException.java
@@ -1,7 +1,7 @@
 package dev.francisco_hernandez.prueba_tecnica.exceptions;
 
-public class BadRequestException extends RuntimeException  {
-    public BadRequestException(String message) {
+public class MethodArgumentNotValidException extends RuntimeException  {
+    public MethodArgumentNotValidException(String message) {
         super(message);
     }
 }
