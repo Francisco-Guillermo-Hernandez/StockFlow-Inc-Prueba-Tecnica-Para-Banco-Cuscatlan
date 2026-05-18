4fdfa6cf75545b60424f6d9c8819b1ea141fbb1e
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-18 14:23:54 -0600

    ft: se implemento el componente de details

--- inventory-app/src/app/alerts/details/details.ts
+++ inventory-app/src/app/alerts/details/details.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Alert } from '~/types/alert';
+import { Alerts } from '~/services/alerts';
+import { Loader } from "~/services/loader/loader";
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [Loader],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
-export class Details {}
+export class Details implements OnInit {
+  public alerts = signal<Array<Alert>>([]);
+  public isLoading = signal<boolean>(false);
+  public error = null;
+  constructor(private readonly service: Alerts) {}
+
+  public ngOnInit(): void {
+    this.listAll();
+  }
+
+  public listAll(): void {
+    this.isLoading.set(true);
+    this.service.listAll().subscribe({
+      next: (data) => {
+        this.alerts.set(data);
+        this.isLoading.set(false);
+      },
+      error: (error) => {
+        console.error(error);
+        this.isLoading.set(false);
+        this.error = error;
+      },
+    });
+  }
+}
