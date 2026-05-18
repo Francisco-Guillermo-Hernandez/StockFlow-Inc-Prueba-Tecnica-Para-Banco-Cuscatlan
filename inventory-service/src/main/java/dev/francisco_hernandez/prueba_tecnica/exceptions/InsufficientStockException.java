0fae264c366e54bf5cc7b695e65b359851a48b82
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-17 22:19:50 -0600

    ft: se definio la excepcion para stock insuficiente

---
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/exceptions/InsufficientStockException.java
@@ -0,0 +1,8 @@
+package dev.francisco_hernandez.prueba_tecnica.exceptions;
+
+public class InsufficientStockException extends Exception {
+
+    public InsufficientStockException() {
+        super("Stock Insuficiente");
+    }
+}
