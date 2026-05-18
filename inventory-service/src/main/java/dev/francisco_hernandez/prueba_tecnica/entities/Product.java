132f780b043a8858cacd53febec39a532d5bb21c
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-17 21:24:31 -0600

    ft: se definieron los campos necesarios para la tabla

--- inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/entities/Product.java
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/entities/Product.java
@@ -1,4 +1,51 @@
 package dev.francisco_hernandez.prueba_tecnica.entities;
 
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String sku;
+
+    @Column(length = 20)
+    private String name;
+
+    @Column(length = 100)
+    private String description;
+
+    @Column(nullable = false)
+    private Integer currentStock;
+
+    @Column(nullable = false)
+    private Integer minStock;
+
+    @Column(nullable = false)
+    private Float unitPrice;
+
+    @Column(nullable = true)
+    private Float weight;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
 }
