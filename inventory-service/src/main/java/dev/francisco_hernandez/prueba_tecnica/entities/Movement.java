95c4ca60af7133aa7528a5925f234f0cc87f2363
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-18 06:21:37 -0600

    ft: Se implemento la entidad Movement

--- inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/entities/Movement.java
+++ inventory-service/src/main/java/dev/francisco_hernandez/prueba_tecnica/entities/Movement.java
@@ -1,4 +1,32 @@
 package dev.francisco_hernandez.prueba_tecnica.entities;
 
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "movements")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long productId;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private String reason;
+
+    @Column(nullable = false)
+    private String operationName;
+
+    private LocalDateTime timestamp;
 }
